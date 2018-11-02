6d89f4e5cfeb10052cd30d7a9c859fb5df0bebdc
Eniz Bilgin <eniz.bilgin@veripark.com>
2018-11-02 13:47:07 +0300

    Activity module updated

--- app/src/main/java/com/enzz/koin/app/implementation/AppPresenterImpl.kt
+++ app/src/main/java/com/enzz/koin/app/implementation/AppPresenterImpl.kt
@@ -1,28 +1,34 @@
-package com.enzz.core.application.implementations
+package com.enzz.koin.app.implementation
 
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.enzz.core.application.interfaces.Configuration
 import com.enzz.core.application.interfaces.Presenter
+import com.enzz.koin.R
+
+/**
+ * Created by eniz.bilgin on 2.11.2018.
+ */
 
 
-class PresenterImpl(private val context: AppCompatActivity, val configuration: Configuration) : Presenter {
+class AppPresenterImpl(private val context: AppCompatActivity, private val configuration: Configuration) : Presenter {
 
     override fun showAlert(): String {
 
-        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context, R.style.AppCompatAlertDialogStyle)
 
         builder.setTitle("Koin")
                 .setMessage("Are you like KOIN")
-                .setPositiveButton(android.R.string.yes, { dialog, which ->
+                .setPositiveButton(android.R.string.yes) { dialog, which ->
                     // continue with delete
-                })
-                .setNegativeButton(android.R.string.no, { dialog, which ->
+                }
+                .setNegativeButton(android.R.string.no) { dialog, which ->
                     // do nothing
-                })
+                }
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show()
 
         return configuration.sayMyName()
     }
+
 }
--- app/src/main/java/com/enzz/koin/di/Module.kt
+++ app/src/main/java/com/enzz/koin/di/Module.kt
@@ -1,8 +1,10 @@
 package com.enzz.koin.di
 
 import com.enzz.core.application.interfaces.HttpClient
+import com.enzz.core.application.interfaces.Presenter
 import com.enzz.core.presentation.activities.AbstractActivity
 import com.enzz.koin.app.implementation.AppHttpClient
+import com.enzz.koin.app.implementation.AppPresenterImpl
 import com.enzz.koin.app.implementation.FragmentManagementImpl
 import com.enzz.koin.app.interfaces.FragmentManagement
 import org.koin.dsl.module.module
@@ -14,8 +16,10 @@ val appModule = module(override = true) {
 
 }
 
-val activityModule = module {
+val activityModule = module(override = true) {
 
     factory<FragmentManagement> { (activity: AbstractActivity) -> FragmentManagementImpl(activity) }
+
+    factory<Presenter> { (activity: AbstractActivity) -> AppPresenterImpl(activity, get()) }
 }
 
